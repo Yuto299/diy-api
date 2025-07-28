d297049aa07e79e4bcef7d83dca9b69636a57169
Yuto299 <ota.se.ke@outlook.jp>
2025-07-29 08:18:58 +0900

    explain edit

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ app.delete("/jokes/:id", (req, res) => {
   const id = parseInt(req.params.id);
   const searchIndex = jokes.findIndex((joke) => joke.id === id);
   if (searchIndex > -1) {
-    // 見つからない場合を正確に判断するため
+    // 見つからない場合を正確に判断するため、配列は0から始まるから（今回は配列になってる）
     jokes.splice(searchIndex, 1);
     res.sendStatus(200);
   } else {
